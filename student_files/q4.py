11b083ac0cce33b02fdeb998833d8e793d15deb5
lonelyhat <avalonglory@gmail.com>
2023-04-15 20:53:18 +0800

    Q4

--- student_files/q4.py
+++ student_files/q4.py
@@ -1,6 +1,7 @@
 import sys
 from pyspark.sql import SparkSession
-
+from pyspark.sql.functions import split, regexp_replace, explode, count, col,trim
+import ast
 # you may add more import if you need to
 
 
@@ -9,3 +10,23 @@ hdfs_nn = sys.argv[1]
 
 spark = SparkSession.builder.appName("Assigment 2 Question 4").getOrCreate()
 # YOUR CODE GOES BELOW
+df = (
+    spark.read.option("header", True)
+    .option("inferSchema", True)
+    .option("delimiter", ",")
+    .option("quotes", '"')
+    .csv("hdfs://%s:9000/assignment2/part1/input/" % (hdfs_nn))
+)
+
+df = df.withColumn("Cuisine Style", col("Cuisine Style").strip(']['))
+df = df.withColumn("Cuisine Style", col("Cuisine Style").split(", "))
+df = df.withColumn("Cuisine Style", col("Cuisine Style").strip('\''))
+result = df.select("City","Cuisine Style")
+result = result.groupBy("City", "Cuisine Style").agg(count("*").alias("count")).sort(col("City").asc(), col("count").desc())
+
+
+result.show()
+
+result.write.csv(
+    "hdfs://%s:9000/assignment2/output/question4/" % (hdfs_nn), header=True
+)
