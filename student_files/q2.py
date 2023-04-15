0fc6b98a179778e8b0e562d0e9e65dea310d4bd6
lingaoliu105 <lingaoliu105@gmail.com>
2023-04-15 20:13:29 +0800

    q2

--- student_files/q2.py
+++ student_files/q2.py
@@ -1,6 +1,7 @@
 import sys
 from pyspark.sql import SparkSession
-
+from pyspark.sql import col,max,min
+from pandas import DataFrame
 # you may add more import if you need to
 
 
@@ -9,3 +10,19 @@ hdfs_nn = sys.argv[1]
 
 spark = SparkSession.builder.appName("Assigment 2 Question 2").getOrCreate()
 # YOUR CODE GOES BELOW
+df:DataFrame = (
+    spark.read.option("header", True)
+    .option("inferSchema", True)
+    .option("delimiter", ",")
+    .option("quotes", '"')
+    .csv("hdfs://%s:9000/assignment2/part1/input/" % (hdfs_nn))
+)
+
+df_good = df.groupby(["Price Range","City"]).agg(max("Rating")).withColumn("Rating")
+df_bad = df.groupby(["Price Range", "City"]).agg(
+    min("Rating")).withColumn("Rating")
+df_both = df_good.union(df_bad)
+df_result = df_both.join(df,on=["Price Range","City","Rating"],how="inner")
+df_result.show()
+df_result.write.csv(
+    "hdfs://%s:9000/assignment2/output/question1/" % (hdfs_nn), header=True)
