b35e74ae0880316e2af91ef636aa653ec0531a96
lingaoliu105 <lingaoliu105@gmail.com>
2023-04-15 19:42:26 +0800

    question 1

--- student_files/q1.py
+++ student_files/q1.py
@@ -1,5 +1,7 @@
 import sys
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+from pyspark.sql.types import BooleanType
 # you may add more import if you need to
 
 
@@ -8,4 +10,26 @@ hdfs_nn = sys.argv[1]
 
 spark = SparkSession.builder.appName("Assigment 2 Question 1").getOrCreate()
 # YOUR CODE GOES BELOW
+def is_valid(col):
+    if col:
+        return bool(eval(col))
+    return False
+# Read file
+df = (
+    spark.read.option("header", True)
+    .option("inferSchema", True)
+    .option("delimiter", ",")
+    .option("quotes", '"')
+    .csv("hdfs://%s:9000/assignment2/part1/input/" % (hdfs_nn))
+)
+
+
+df_filtered = df.filter((col("Rating") >= 1.0) & (
+    col("Rating").isNotNull()) & (is_valid(col("Reviews"))))
+
+df_filtered.show()
+
+# Write file
+df_filtered.write.csv(
+    "hdfs://%s:9000/assignment2/output/question1/" % (hdfs_nn), header=True)
 
