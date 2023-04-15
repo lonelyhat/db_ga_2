44e995f34a7fac717f3c4efc01857c3b495dd4f6
Ziniu <guo.ziniu.1003@gmail.com>
2023-04-15 21:04:47 +0800

    feat: q3 not verified

--- student_files/q3.py
+++ student_files/q3.py
@@ -1,5 +1,6 @@
 import sys
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json, explode
 
 # you may add more import if you need to
 
@@ -9,3 +10,21 @@ hdfs_nn = sys.argv[1]
 
 spark = SparkSession.builder.appName("Assigment 2 Question 3").getOrCreate()
 # YOUR CODE GOES BELOW
+json_schema = "array<struct<ReviewContent:string, Date:string>>"
+df = (
+    spark.read.option("header", True)
+    .option("inferSchema", True)
+    .option("delimiter", ",")
+    .option("quotes", '"')
+    .csv("hdfs://%s:9000/assignment2/part1/input/" % (hdfs_nn))
+)
+df = df.select(["ID_TA", "Reviews"])
+df_json = df.withColumn("Reviews", from_json("Reviews", json_schema))
+df_flattened = df_json.select("ID_TA", explode("Reviews").alias("Reviews"))
+df_new = df_flattened.select("ID", "Review.ReviewContent as Review", "Review.Date as Date")
+df_new.show()
+df_new.write.csv(
+    "hdfs://%s:9000/assignment2/output/question3/" % (hdfs_nn),
+    header=True,
+    emptyValue="",
+)
